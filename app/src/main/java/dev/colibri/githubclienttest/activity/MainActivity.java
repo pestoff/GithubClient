6fb60ddaeb118de111dc89f8e5197a8185ff5997
Alexey Misiulia <alexeymisiulia@gmail.com>
2019-02-28 15:43:49 +0300

    Продвинутый модуль. Добавление RepoListFragment

--- app/src/main/java/dev/colibri/githubclienttest/activity/MainActivity.java
+++ app/src/main/java/dev/colibri/githubclienttest/activity/MainActivity.java
@@ -1,80 +1,57 @@
 package dev.colibri.githubclienttest.activity;
 
-import java.io.IOException;
-import java.util.ArrayList;
-
-import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.ProgressBar;
+import android.widget.ImageView;
 import android.widget.Toast;
 
-import org.json.JSONException;
-
 import dev.colibri.githubclienttest.R;
-import dev.colibri.githubclienttest.adapter.RepositoryAdapter;
-import dev.colibri.githubclienttest.entity.Repository;
-import dev.colibri.githubclienttest.network.HttpClient;
+import dev.colibri.githubclienttest.fragment.RepoListFragment;
 
 public class MainActivity extends AppCompatActivity {
     private static final String LOG_TAG = "dc.MainActivity";
-
-    private HttpClient httpClient;
-    private RepositoryAdapter repositoryAdapter;
-    private EditText queryEditText;
-    private ProgressBar progressBar;
-    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        initRecyclerView();
+        if(savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.master_fragment_container, new RepoListFragment(), RepoListFragment.TAG)
+                    .commit();
+        }
 
-        queryEditText = findViewById(R.id.query_edit_text);
-        progressBar = findViewById(R.id.progress_bar);
-        toolbar = findViewById(R.id.toolbar);
+        initView();
+    }
+
+    private void initView() {
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        httpClient = new HttpClient();
-    }
+        final ImageView searchImageView = findViewById(R.id.search_button);
+        final EditText searchEditText = findViewById(R.id.query_edit_text);
 
-    private void initRecyclerView() {
-        RecyclerView recyclerView = findViewById(R.id.repository_recycler_view);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        RepositoryAdapter.OnRepoClickListener listener = new RepositoryAdapter.OnRepoClickListener() {
+        searchImageView.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onRepoClick(Repository repository) {
-                Intent intent = new Intent(MainActivity.this, RepoDetailsActivity.class);
-                intent.putExtra(RepoDetailsActivity.EXTRA_REPO_NAME, repository.getName());
-                String userLogin = repository.getOwner().getLogin();
-                intent.putExtra(RepoDetailsActivity.EXTRA_USER_LOGIN, userLogin);
-                startActivity(intent);
-            }
-        };
-        repositoryAdapter = new RepositoryAdapter(listener);
-        recyclerView.setAdapter(repositoryAdapter);
-    }
+            public void onClick(View v) {
+                Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.master_fragment_container);
+                final RepoListFragment repoListFragment = (RepoListFragment) fragment;
+                if(repoListFragment == null) throw new NullPointerException("Fragment can't be null");
 
-    public void searchRepositories(View v) {
-        String query = queryEditText.getText().toString();
-        if(query.isEmpty()) {
-            Toast.makeText(this, R.string.empty_text, Toast.LENGTH_SHORT).show();
-        } else {
-            repositoryAdapter.clearItems();
-            new GetRepositoriesAsyncTask().execute(query);
-        }
+                String query = searchEditText.getText().toString();
+                repoListFragment.onSearchQueryChanged(query);
+            }
+        });
     }
 
     @Override
@@ -88,41 +65,11 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.settings:
-                Toast.makeText(this, R.string.settings_clicked_hint, Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "TODO: add settings screen", Toast.LENGTH_SHORT).show();
                 return true;
 
             default:
                 return super.onOptionsItemSelected(item);
-        }
-    }
-
-    private class GetRepositoriesAsyncTask extends AsyncTask<String, Void, ArrayList<Repository>> {
-
-        @Override
-        protected void onPreExecute() {
-            progressBar.setVisibility(View.VISIBLE);
-        }
-
-        @Override
-        protected ArrayList<Repository> doInBackground(String... queries) {
-
-            try {
-                return httpClient.getRepositories(queries[0]);
-            } catch (IOException e) {
-                e.printStackTrace();
-                return null;
-            }
-        }
-
-        @Override
-        protected void onPostExecute(ArrayList<Repository> result) {
-            progressBar.setVisibility(View.GONE);
-
-            if(result != null) {
-                repositoryAdapter.addItems(result);
-            } else {
-                Toast.makeText(MainActivity.this, R.string.error_msg, Toast.LENGTH_SHORT).show();
-            }
         }
     }
 }
---
+++ app/src/main/java/dev/colibri/githubclienttest/fragment/RepoListFragment.java
@@ -0,0 +1,92 @@
+package dev.colibri.githubclienttest.fragment;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import dev.colibri.githubclienttest.R;
+import dev.colibri.githubclienttest.adapter.RepositoryAdapter;
+import dev.colibri.githubclienttest.entity.Repository;
+import dev.colibri.githubclienttest.network.HttpClient;
+
+public class RepoListFragment extends Fragment {
+    public static final String TAG = "RepoListFragment";
+
+    private HttpClient httpClient;
+    private RepositoryAdapter repositoryAdapter;
+    private ProgressBar progressBar;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_repo_list, container, false);
+
+        initRecyclerView(view);
+
+        progressBar = view.findViewById(R.id.progress_bar);
+        httpClient = new HttpClient();
+        return view;
+    }
+
+    private void initRecyclerView(View view) {
+        RecyclerView recyclerView = view.findViewById(R.id.repository_recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        RepositoryAdapter.OnRepoClickListener listener = (repository) -> {
+                // TODO: add action
+        };
+        repositoryAdapter = new RepositoryAdapter(listener);
+        recyclerView.setAdapter(repositoryAdapter);
+    }
+
+    public void onSearchQueryChanged(String query) {
+        if(query.isEmpty()) {
+            Toast.makeText(getActivity(), R.string.empty_text, Toast.LENGTH_SHORT).show();
+        } else {
+            repositoryAdapter.clearItems();
+            new GetRepositoriesAsyncTask().execute(query);
+        }
+    }
+
+    private class GetRepositoriesAsyncTask extends AsyncTask<String, Void, ArrayList<Repository>> {
+
+        @Override
+        protected void onPreExecute() {
+            progressBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected ArrayList<Repository> doInBackground(String... queries) {
+
+            try {
+                return httpClient.getRepositories(queries[0]);
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<Repository> result) {
+            progressBar.setVisibility(View.GONE);
+
+            if(result != null) {
+                repositoryAdapter.addItems(result);
+            } else {
+                Toast.makeText(getActivity(), R.string.error_msg, Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
