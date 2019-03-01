de98e20c572d30a21c17bb635917ae36a98b9616
Alexey Misiulia <alexeymisiulia@gmail.com>
2019-03-01 08:36:30 +0300

    [ADD] App, DataRepository classes

---
+++ app/src/main/java/dev/colibri/githubclienttest/App.java
@@ -0,0 +1,44 @@
+package dev.colibri.githubclienttest;
+
+import android.app.Application;
+import android.arch.persistence.room.Room;
+
+import dev.colibri.githubclienttest.db.AppDatabase;
+import dev.colibri.githubclienttest.network.HttpClient;
+import dev.colibri.githubclienttest.repository.DataRepository;
+
+public class App extends Application {
+    private static HttpClient httpClient;
+    private static AppDatabase appDatabase;
+    private static DataRepository dataRepository;
+    private static App INSTANCE;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        INSTANCE = this;
+    }
+
+    public static DataRepository getDataRepository() {
+        if(dataRepository == null) {
+            dataRepository = new DataRepository();
+        }
+
+        return dataRepository;
+    }
+
+    public static HttpClient getHttpClient() {
+        if(httpClient == null) {
+            httpClient = new HttpClient();
+        }
+        return httpClient;
+    }
+
+    public static AppDatabase getAppDatabase() {
+        if(appDatabase == null) {
+            appDatabase = Room.databaseBuilder(INSTANCE, AppDatabase.class, AppDatabase.DB_NAME).build();
+        }
+        return appDatabase;
+    }
+}
+
---
+++ app/src/main/java/dev/colibri/githubclienttest/repository/DataRepository.java
@@ -0,0 +1,37 @@
+package dev.colibri.githubclienttest.repository;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import dev.colibri.githubclienttest.App;
+import dev.colibri.githubclienttest.db.AppDatabase;
+import dev.colibri.githubclienttest.entity.Repository;
+import dev.colibri.githubclienttest.network.HttpClient;
+
+public class DataRepository {
+    private HttpClient httpClient = App.getHttpClient();
+    private AppDatabase db = App.getAppDatabase();
+
+    public Repository getRepository(String repoName, String userLogin) throws IOException {
+        Repository repository = db.repositoryDao().getRepository(repoName, userLogin);
+        if (repository == null) throw new IOException("Can't find repository entity in db");
+
+        return repository;
+    }
+
+    public ArrayList<Repository> getRepositories(String query) throws IOException {
+        ArrayList<Repository> repositories = null;
+        try {
+
+            repositories = httpClient.getRepositories(query);
+            db.repositoryDao().insertRepositories(repositories);
+        } catch (IOException e) {
+            String dbWildCardQuery = "%" + query + "%";
+            repositories = new ArrayList<>(db.repositoryDao().getRepositories(dbWildCardQuery));
+        }
+
+        if(repositories == null) throw new IOException("Can't find repositories entities in db or api");
+
+        return repositories;
+    }
+}
