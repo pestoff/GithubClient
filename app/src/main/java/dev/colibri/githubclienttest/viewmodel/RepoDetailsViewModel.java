d31504027f760e03ab4fb7682361361ae919d981
Alexey Misiulia <alexeymisiulia@gmail.com>
2019-03-04 15:49:53 +0300

    [REF] remove LiveData lazy initialization. Remove resetting error logic

--- app/src/main/java/dev/colibri/githubclienttest/viewmodel/RepoDetailsViewModel.java
+++ app/src/main/java/dev/colibri/githubclienttest/viewmodel/RepoDetailsViewModel.java
@@ -15,30 +15,21 @@ public class RepoDetailsViewModel extends ViewModel {
 
     private DataRepository dataRepository = App.getDataRepository();
 
-    private MutableLiveData<Repository> repository;
-    private MutableLiveData<Boolean> isNetworkException;
-    private MutableLiveData<Boolean> isLoading;
+    private MutableLiveData<Repository> repository = new MutableLiveData<>();
+    private MutableLiveData<Boolean> isNetworkException = new MutableLiveData<>();
+    private MutableLiveData<Boolean> isLoading = new MutableLiveData<>();
 
 
     public LiveData<Repository> getRepository() {
-        if (repository == null) {
-            repository = new MutableLiveData<>();
-        }
         return repository;
     }
 
 
     public LiveData<Boolean> isException() {
-        if(isNetworkException == null) {
-            isNetworkException = new MutableLiveData<>();
-        }
         return isNetworkException;
     }
 
     public LiveData<Boolean> isLoading() {
-        if(isLoading == null) {
-            isLoading = new MutableLiveData<>();
-        }
         return isLoading;
     }
 
@@ -74,9 +65,7 @@ public class RepoDetailsViewModel extends ViewModel {
                 repository.setValue(result);
             }
             else {
-                // don't want value to be saved after screen rotation
                 isNetworkException.setValue(true);
-                isNetworkException.setValue(false);
             }
         }
     }
